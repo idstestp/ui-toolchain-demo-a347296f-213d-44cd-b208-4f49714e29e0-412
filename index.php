b2fd13fe98cbe9672f3ecb47e7f71d0b64e9893d
load0503t001 Tester <bluemix_ui_load0503t001@mailinator.com>
2016-06-02 14:35:37 +0000

    Committed by Jmeter sampler at 1464878121996

--- index.php
+++ index.php
@@ -85,13 +85,13 @@ function orderItem(itemID){
 </script>
 <html>
 <head>
-	<title>Jmeter updated at 1 1464876224536</title>
+	<title>Jmeter updated at 1 1464878106367</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 	<link rel="stylesheet" href="style.css">
 </head>
 <table class="headerTable">
 	<tr>
-		<td><span class="pageTitle"><h1>Jmeter updated at 1 1464876224536</h1></span></td> 
+		<td><span class="pageTitle"><h1>Jmeter updated at 1 1464878106367</h1></span></td> 
 	</tr>
 </table>
 <body onload="loadItems(items)">
